d1f491fa2662af9fe9d045b2d2d0e182df1b9d04
Cristiano dos Santos Lagame <cristiano.lagame@fgv.br>
2021-07-08 13:33:38 -0300

    commit inicial

---
+++ Assets/Scripts/GestorDeRede.cs
@@ -0,0 +1,30 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestorDeRede : MonoBehaviourPunCallbacks
+{
+    public static GestorDeRede Instancia { get; private set; }
+
+    private void Awake() 
+    {
+        if(Instancia != null && Instancia != this)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        Instancia = this;
+        DontDestroyOnLoad(gameObject);
+    }   
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conexão bem sucedida!");
+    }
+}
